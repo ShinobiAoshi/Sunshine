45a0fc9aab1f04196307a2822d448873fcb4328b
Stephen Liles <shinobiaoshi@gmail.com>
2015-08-30 22:05:24 -0500

    get tablet UI functional

--- app/src/main/java/com/example/stephen/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/stephen/sunshine/app/ForecastFragment.java
@@ -1,6 +1,5 @@
 package com.example.stephen.sunshine.app;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -61,6 +60,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private ForecastAdapter mForecastAdapter;
 
+    public interface Callback {
+        public void onItemSelected(Uri dateUri);
+    }
+
     public ForecastFragment()
     {
 
@@ -111,11 +114,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
                 Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
                 if (cursor != null) {
                     String locationSetting = Utility.getPreferredLocation(getActivity());
-                    Intent intent = new Intent(getActivity(), DetailActivity.class)
-                            .setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
-                                    locationSetting, cursor.getLong(COL_WEATHER_DATE)
-                            ));
-                    startActivity(intent);
+                    ((Callback) getActivity()).onItemSelected(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
+                            locationSetting, cursor.getLong(COL_WEATHER_DATE)));
                 }
             }
         });
