16726fe2e1cdcd7eb1929ebba8f7db98d3121068
Stephen Liles <shinobiaoshi@gmail.com>
2015-08-31 22:39:19 -0500

    update app to use service in background

--- app/src/main/java/com/example/stephen/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/stephen/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.stephen.sunshine.app;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.stephen.sunshine.app.data.WeatherContract;
+import com.example.stephen.sunshine.app.service.SunshineService;
 
 /**
  * Created by Stephen on 2/23/2015.
@@ -147,9 +149,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private void updateWeather()
     {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     public void setUseTodayLayout(boolean useTodayLayout) {
